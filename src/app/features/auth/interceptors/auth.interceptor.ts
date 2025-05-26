e8def26ef5b95e44880a2036ed7609973e7fa251
ShadowCatP <282276@student.pwr.edu.pl>
2025-05-26 17:37:16 +0200

    added auth interceptor

---
+++ src/app/features/auth/interceptors/auth.interceptor.ts
@@ -0,0 +1,13 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = inject(AuthService).getToken();
+
+  const newReq = req.clone({
+    headers: req.headers.append('Authorization', `Bearer ${token}`),
+  });
+
+  return next(newReq);
+};
