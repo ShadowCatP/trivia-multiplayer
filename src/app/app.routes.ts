71f30e60dff9fa8abf9f77b62627bced15546b5f
ShadowCatP <282276@student.pwr.edu.pl>
2025-05-26 19:20:37 +0200

    added login form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/auth/components/register/register.component';
+import { LoginComponent } from './features/auth/components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+];
---
+++ src/app/features/auth/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    this.auth.login(this.form.getRawValue()).subscribe({
+      next: (res) => {
+        console.log('Response', res);
+      },
+    });
+  }
+}
