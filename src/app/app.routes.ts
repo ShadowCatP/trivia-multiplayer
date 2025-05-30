5f1b1a1aae2f2cbcd17e54732ff1bd881a3dc94a
ShadowCatP <282276@student.pwr.edu.pl>
2025-05-30 13:22:12 +0200

    added header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/components/register/register.component';
 import { LoginComponent } from './features/auth/components/login/login.component';
-import { AppComponent } from './app.component';
 import { authGuard } from './features/auth/guards/auth.guard';
+import { LogoutComponent } from './shared/components/logout/logout.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', component: AppComponent, canActivate: [authGuard] },
+  { path: '', component: LogoutComponent, canActivate: [authGuard] },
 ];
