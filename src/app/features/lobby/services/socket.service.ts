dc568a120cdd767649b635905ca1ad5c68ffb4e9
ShadowCatP <282276@student.pwr.edu.pl>
2025-07-15 21:40:26 +0200

    fix: socket auth problems, now we send token on connect

--- src/app/features/lobby/services/socket.service.ts
+++ src/app/features/lobby/services/socket.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
+import { TokenService } from '../../auth/services/token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SocketService {
+  private readonly tokenService = inject(TokenService);
   public socket: Socket;
 
   constructor() {
-    this.socket = io('http://localhost:5001');
+    const token = this.tokenService.accessToken();
+
+    this.socket = io('http://localhost:5001', {
+      query: { token },
+    });
+
+    this.socket.on('connect_error', (err) => {
+      console.error('Socket connection error:', err.message);
+    });
   }
 }
